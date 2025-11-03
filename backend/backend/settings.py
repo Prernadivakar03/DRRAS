250217312408a8c5f667aaae02c08050a7236826
Prernadivakar03 <prernadivakar0328@gmail.com>
2025-11-03 13:42:27 +0530

    Fixed api module import for Render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'api',
+    'backend.api',
 ]
 
 # ✅ CORS SETTINGS (Place outside INSTALLED_APPS)
